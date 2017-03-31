c24f6d93e85cddf538d12d5841aedf7279c9e2ce
Yamil Asusta <hello@yamilasusta.com>
2017-03-31 12:50:09 -0500

    Add TF files

---
+++ infra/main.tf
@@ -0,0 +1,48 @@
+resource "digitalocean_loadbalancer" "platzi-demo" {
+  name   = "platzi-html-v2"
+  region = "nyc3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+  }
+
+  droplet_tag = "${digitalocean_tag.platzi-demo.name}"
+}
+
+resource "digitalocean_tag" "platzi-demo" {
+  name = "platzi-html"
+}
+
+resource "digitalocean_droplet" "platzi-demo" {
+  count    = 3
+  image    = "23826948"
+  name     = "platzi-demo-v2"
+  region   = "nyc3"
+  size     = "512mb"
+  ssh_keys = [7741403]
+  tags     = ["${digitalocean_tag.platzi-demo.id}"]
+
+  user_data = <<EOF
+#cloud-config
+coreos:
+  units:
+    - name: "platzi.service"
+      command: "start"
+      content: |
+        [Unit]
+        Description=Platzi Demo
+        After=docker.service
+
+        [Service]
+        ExecStart=/usr/bin/docker run -d -p 3000:3000 platzi
+EOF
+}
